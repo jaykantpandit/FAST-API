0c7ebe5d153932612f02319be67aeca77cd7108d
Jaykant Pandit <jaykantpandit@Jaykants-MacBook-Air.local>
2026-04-16 12:03:14 +0530

    added sql path

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:123456789@127.0.0.1:3306/TodoApplicationDatabase'#'postgresql://postgres:123456@localhost/TodoApplicationDatabase'#'sqlite:///./todosapp.db' # db path in this application
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db' #'mysql+pymysql://root:123456789@127.0.0.1:3306/TodoApplicationDatabase'#'postgresql://postgres:123456@localhost/TodoApplicationDatabase'#'sqlite:///./todosapp.db' # db path in this application
 
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
